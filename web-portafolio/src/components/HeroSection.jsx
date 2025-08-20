68b8ef767a63f873564edbaa9c57eb2ec37456f5
Nico Barberis <nico09barberis@gmail.com>
2025-08-20 13:10:08 -0300

    nuevo enlace de contacto en los datos adicionales

--- web-portafolio/src/components/HeroSection.jsx
+++ web-portafolio/src/components/HeroSection.jsx
@@ -1,6 +1,5 @@
-import ProfileImagen from "../assets/img-hero.png";
 import Nav from "./Nav";
-import { Rss, Cake, BriefcaseBusiness, CalendarDays, Link } from "lucide-react";
+import { Rss, Cake, BriefcaseBusiness, Mail, Link } from "lucide-react";
 import SubscribeForm from "./Subs";
 
 const HeroSection = () => {
@@ -12,16 +11,21 @@ const HeroSection = () => {
           {/* Botón Seguir */}
           <div className="flex justify-end mb-4 pr-4 md:pr-12">
             <button className="flex items-center gap-2 bg-pink-600 px-4 py-2 rounded-full cursor-pointer hover:scale-105 transition">
-              <span className="text-white text-lg font-bold tracking-wide">Seguir</span>
+              <span className="text-white text-lg font-bold tracking-wide">
+                Seguir
+              </span>
               <Rss size={20} color="white" />
             </button>
           </div>
 
           <div className="text-left mt-12 pl-4 md:pl-12">
             <h2 className="text-3xl font-bold">Nicolas Barberis</h2>
-            <p className="text-xl font-semibold mt-1">Técnico Universitario en Programación</p>
+            <p className="text-xl font-semibold mt-1">
+              Técnico Universitario en Programación
+            </p>
             <p className="font-semibold mt-2">
-              Apasionado por la tecnología y la automatización, creo soluciones funcionales con código limpio y eficiente.
+              Apasionado por la tecnología y la automatización, creo soluciones
+              funcionales con código limpio y eficiente.
             </p>
 
             {/* Datos adicionales */}
@@ -34,10 +38,13 @@ const HeroSection = () => {
                 <BriefcaseBusiness size={20} />
                 <span>500Millas</span>
               </div>
-              <div className="flex items-center gap-1">
-                <CalendarDays size={20} />
-                <span>Te uniste en Jun '22</span>
-              </div>
+              <a
+                href="mailto:barberisnicolas.dev@gmail.com"
+                className="flex items-center gap-1"
+              >
+                <Mail size={20} aria-hidden="true" />
+                <span>Contacto</span>
+              </a>
               <div className="flex items-center gap-1">
                 <Cake size={20} />
                 <span>7 Nov. 2002</span>
